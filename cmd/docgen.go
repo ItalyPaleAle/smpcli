1d05ce55b05d822d855eac8138d2b3e7b6e33e77
Alessandro Segala <ItalyPaleAle@users.noreply.github.com>
2019-07-07 23:26:04 -0700

    Added command to generate Markdown docs

---
+++ cmd/docgen.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2019 Alessandro Segala (@ItalyPaleAle)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// +build generator
+
+// This file is built only when trying to generate the documentation for the CLI
+// go run -tags generator .
+
+package cmd
+
+import (
+	"github.com/spf13/cobra/doc"
+)
+
+func init() {
+	err := doc.GenMarkdownTree(rootCmd, "./docs")
+	if err != nil {
+		panic(err)
+	}
+}
